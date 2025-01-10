a364d78d40dc4c0d090c9f9d03358e90c457ac36
GonzaQu3dena <gonzalo.qu3dena@gmail.com>
2025-01-10 18:17:36 -0500

    refactor(public): add route to cubit counter screen.

--- lib/public/home/screens/home_screen.dart
+++ lib/public/home/screens/home_screen.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
 
-  final Widget childView;
-
   const HomeScreen({
     super.key,
-    required this.childView,
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: childView,
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text('Cubits'),
+            subtitle: const Text('Simple state management'),
+            trailing: const Icon(
+              Icons.arrow_forward_ios_rounded,
+            ),
+            onTap: () => {
+              context.push('/cubits')
+            },
+          )
+        ],
+      ),
     );
   }
 }
