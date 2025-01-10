5d9d2ec97cc0044c37cab2e93f5fec0ce913dd37
GonzaQu3dena <gonzalo.qu3dena@gmail.com>
2025-01-10 18:16:12 -0500

    refactor: change counter page name and change route implementation.

--- lib/counter/presentation/screens/cubit_counter_screen.dart
+++ lib/counter/presentation/screens/cubit_counter_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
-  const CounterScreen({super.key});
+class CubitCounterScreen extends StatelessWidget {
+  const CubitCounterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/shared/router/app_router.dart
+++ lib/shared/router/app_router.dart
@@ -1,20 +1,17 @@
-import 'package:forms_app/counter/presentation/screens/counter_screen.dart';
 import 'package:go_router/go_router.dart';
 import 'package:forms_app/public/home/screens/home_screen.dart';
+import 'package:forms_app/counter/presentation/screens/cubit_counter_screen.dart';
 
-final appRouter = GoRouter(initialLocation: '/', routes: [
-  ShellRoute(
-    builder: (context, state, child) {
-      return HomeScreen(childView: child);
-    },
-    
-    routes: [
-      GoRoute(
-        path: '/',
-        builder: (context, state) {
-          return const CounterScreen();
-        },
-      )
-    ],
-  )
-]);
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/cubits',
+      builder: (context, state) => const CubitCounterScreen(),
+    )
+  ],
+);
