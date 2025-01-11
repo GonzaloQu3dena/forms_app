43c40599374689b1620c11de6e3e7e35c29615f8
GonzaQu3dena <gonzalo.qu3dena@gmail.com>
2025-01-11 12:16:24 -0500

    refactor(counter): improve cubit counter screen design.

--- lib/counter/presentation/screens/cubit_counter_screen.dart
+++ lib/counter/presentation/screens/cubit_counter_screen.dart
@@ -5,6 +5,45 @@ class CubitCounterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cubit Counter'),
+        actions: [
+          IconButton(
+            onPressed: () => {},
+            icon: Icon(Icons.refresh_rounded),
+          )
+        ],
+      ),
+      body: Center(
+        child: const Text('Counter value : xyz'),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: '1',
+            child: const Text('+3'),
+            onPressed: () => {},
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          FloatingActionButton(
+            heroTag: '2',
+            child: const Text('+2'),
+            onPressed: () => {},
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          FloatingActionButton(
+            heroTag: '3',
+            child: const Text('+1'),
+            onPressed: () => {},
+          ),
+        ],
+      ),
+    );
   }
 }
