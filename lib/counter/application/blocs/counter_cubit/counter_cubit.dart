26ebb49f4a2689a2e664c764f1237c28a02b926b
GonzaQu3dena <gonzalo.qu3dena@gmail.com>
2025-01-12 12:22:04 -0500

    feat(counter): add cubit counter and implementation on screen.

---
+++ lib/counter/application/blocs/counter_cubit/counter_cubit.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'counter_state.dart';
+
+/// ### Counter Cubit
+/// This is the business logic component of the counter feature. It is responsible for managing the state of the counter.
+///
+/// #### Features
+/// - [increaseBy] increases the counter by a given value.
+/// - [reset] resets the counter to 0.
+///
+/// #### Author
+/// Gonzalo Quedena
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterState(counter: 5));
+
+  void increaseBy(int value) {
+    emit(
+      state.copyWith(
+        counter: state.counter + value,
+        transactionCount: state.transactionCount + 1,
+      ),
+    );
+  }
+
+  void reset() {
+    emit(
+      state.copyWith(
+        counter: 0,
+      ),
+    );
+  }
+}
---
+++ lib/counter/application/blocs/counter_cubit/counter_state.dart
@@ -0,0 +1,32 @@
+part of 'counter_cubit.dart';
+
+/// ### Counter State
+/// This class is responsible for holding the state of the counter.
+///
+/// #### Properties
+/// - [counter] - The current value of the counter.
+/// - [transactionCount] - The number of transactions that have been made.
+///
+/// #### Methods
+/// - [copyWith] - A method that creates a new instance of the [CounterState] with the provided values.
+/// 
+/// #### Author
+/// Gonzalo Quedena
+class CounterState {
+  final int counter;
+  final int transactionCount;
+
+  CounterState({
+    this.counter = 0,
+    this.transactionCount = 0,
+  });
+
+  copyWith({
+    int? counter,
+    int? transactionCount,
+  }) =>
+      CounterState(
+        counter: counter ?? this.counter,
+        transactionCount: transactionCount ?? this.transactionCount,
+      );
+}
