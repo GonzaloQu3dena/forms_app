36bfc6f71dad3ed4d2b19d50a7a616430f6a6388
GonzaQu3dena <gonzalo.qu3dena@gmail.com>
2025-01-14 00:06:01 -0500

    feat(public): add option for form management in home view.

--- lib/public/home/screens/home_screen.dart
+++ lib/public/home/screens/home_screen.dart
@@ -3,10 +3,10 @@ import 'package:forms_app/public/home/widgets/custom_list_tile.dart';
 
 /// #### Home Screen
 /// This is the home screen of the app. It contains a list of all the available screens.
-/// 
+///
 /// #### Properties
 /// - [HomeScreen.name] - The name of the screen.
-/// 
+///
 /// #### Author
 /// Gonzalo Quedena
 class HomeScreen extends StatelessWidget {
@@ -31,8 +31,21 @@ class HomeScreen extends StatelessWidget {
             title: 'BLoC',
             subtitle: 'Complex state management',
           ),
+          _divider(),
+          CustomListTile(
+            route: '/',
+            title: 'New User',
+            subtitle: 'Forms management',
+          )
         ],
       ),
+    );
+  }
+
+  Widget _divider() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Divider(),
     );
   }
 }
