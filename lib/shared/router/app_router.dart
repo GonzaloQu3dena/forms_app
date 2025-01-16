0450a775b6db7b5f5a5a6e794543477eab4555fd
GonzaQu3dena <gonzalo.qu3dena@gmail.com>
2025-01-16 16:10:54 -0500

    feat(shared): add route to sign-up screen.

--- lib/shared/router/app_router.dart
+++ lib/shared/router/app_router.dart
@@ -1,6 +1,7 @@
-import 'package:forms_app/counter/presentation/screens/bloc_counter_screen.dart';
 import 'package:go_router/go_router.dart';
 import 'package:forms_app/public/home/screens/home_screen.dart';
+import 'package:forms_app/iam/presentation/screens/sign_up_screen.dart';
+import 'package:forms_app/counter/presentation/screens/bloc_counter_screen.dart';
 import 'package:forms_app/counter/presentation/screens/cubit_counter_screen.dart';
 
 final appRouter = GoRouter(
@@ -17,6 +18,10 @@ final appRouter = GoRouter(
     GoRoute(
       path: '/bloc',
       builder: (context, state) => const BlocCounterScreen(),
-    )
+    ),
+    GoRoute(
+      path: '/sign-up',
+      builder: (context, state) => const SignUpScreen(),
+    ),
   ],
 );
